5c9eaa7f58b58da9195d0c67168d5b78e01c5814
vorsanigo <verorsanigo@gmail.com>
2021-10-31 12:36:37 +0100

    Update csv_to_ttl_occupation.py

--- csv_to_ttl_occupation.py
+++ csv_to_ttl_occupation.py
@@ -1,4 +1,4 @@
-# todo if-else for alt and hidden label -> keep them always to avoid missing results
+# todo use description too  ?
 #import the CSV module for dealing with CSV files
 import csv
 
@@ -25,7 +25,7 @@ for row in reader:
 				str_split_alt += ', \"' + split_alt[el] + '\"'
 			d = '<' + c[1] + '> \n skos:prefLabel "' + c[3] + '" ;\n skos:altLabel "' + str_split_alt
 		else:
-			d = '<' + c[1] + '> \n skos:prefLabel "' + c[3] + '\"'
+			d = '<' + c[1] + '> \n skos:prefLabel "' + c[3] + '" ;\n skos:altLabel ""'
 		if len(split_hidden) != 0:
 			str_split_hidden = split_hidden[0] + '\"'
 			for el in range(1, len(split_hidden), 1):
@@ -34,7 +34,7 @@ for row in reader:
 			#c[12] = c[12].replace("\n", "")
 			#d += ' ;\n skos:hiddenLabel "' + str_split_hidden + ' ;\n dct:description "' + c[12] + '" .\n \n'
 		else:
-			d += ' .\n \n'
+			d += ' ;\n skos:hiddenLabel ""' + ' .\n \n'
 			#c[12] = c[12].replace("\n", "")
 			#d += ' ;\n dct:description "' + c[12] + '" .\n \n'
 		outfile.write(d)	# now write the d variable into the file
