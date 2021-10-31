0c214f9b2db684a728bd915232b611f102974615
vorsanigo <verorsanigo@gmail.com>
2021-10-31 16:43:53 +0100

    Update sparql.py

--- sparql.py
+++ sparql.py
@@ -1,8 +1,13 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import Levenshtein
-import sys, json
-import requests
+import editdistance
+import jaccard_index
+from jaccard_index.jaccard import jaccard_index
+import jaro
+import pickle
 
+
+# todo check similarity string
 
 def sparql_query(query, endpoint):
        '''
@@ -20,6 +25,24 @@ def sparql_query(query, endpoint):
        return output
 
 
+def levenshtein_sim(str1, str2):
+       '''
+       Given two strings, it returns the similarity between them by using levenshtein distance
+       :param str1:
+       :param str2:
+       :return:
+       '''
+       return 1 - (Levenshtein.distance(str1, str2) / max(len(str1), len(str2)))
+
+def edit_sim(str1, str2):
+       '''
+       Given two strings, it returns the similarity between them by using edit distance
+       :param str1:
+       :param str2:
+       :return:
+       '''
+       return 1 - (editdistance.eval(str1, str2) / max(len(str1), len(str2)))
+
 def string_sim(str1, str2, dist_type):
        '''
        Given two strings and a type of distance/similarity, it returns their similarity
@@ -29,13 +52,21 @@ def string_sim(str1, str2, dist_type):
        :return:
        '''
        if dist_type == "levenshtein":
-              return Levenshtein.distance(str1, str2)
+              return levenshtein_sim(str1, str2)
+       elif dist_type == "edit":
+              return edit_sim(str1, str2)
+       elif dist_type == "jaccard":
+              return jaccard_index(str1, str2)
+       elif dist_type == "jaro":
+              return jaro.jaro_winkler_metric(str1, str2)
 
 
-def eval_results(output, entity, threshold, dist_type):
+def eval_results(output, entity, compare, threshold, dist_type, taxonomy_type):
        '''
-       Given the output containing all the labels from the taxonomy, the entity (string) to match and a threshold, it
-       returns the labels with the related skill URI that have similarity with the entity below the threshold
+       Given the output containing all the labels from the taxonomy section of skills and occupations, the entity (string)
+       to match and a threshold, it returns the labels with the related skill URI that have similarity with the entity
+       below the threshold, taxonomy_type (1,2) indicates if we consider the section of occupations and skills or of
+       different skills
        :param output:
        :param entity:
        :param threshold:
@@ -45,17 +76,46 @@ def eval_results(output, entity, threshold, dist_type):
        #print(results)
 
        filter_results = []
-       for binding in results['bindings']:
-              if string_sim(binding['prefLabel']['value'], entity, dist_type) <= threshold or \
-                      string_sim(binding['altLabel']['value'], entity, dist_type) <= threshold or \
-                      string_sim(binding['hiddenLabel']['value'], entity, dist_type) <= threshold: filter_results.append(binding)
+
+       if taxonomy_type == 1:
+              if compare == ">=":
+                     for binding in results['bindings']:
+                            if string_sim(binding['prefLabel']['value'], entity, dist_type) >= threshold or \
+                                    string_sim(binding['altLabel']['value'], entity, dist_type) >= threshold or \
+                                    string_sim(binding['hiddenLabel']['value'], entity, dist_type) >= threshold: filter_results.append(binding)
+              else:
+                     for binding in results['bindings']:
+                            if string_sim(binding['prefLabel']['value'], entity, dist_type) <= threshold or \
+                                    string_sim(binding['altLabel']['value'], entity, dist_type) <= threshold or \
+                                    string_sim(binding['hiddenLabel']['value'], entity,
+                                               dist_type) <= threshold: filter_results.append(binding)
+       else: # taxonomy part with 3 different skills does not have hiddenLabel
+              if compare == ">=":
+                     for binding in results['bindings']:
+                            if string_sim(binding['prefLabel']['value'], entity, dist_type) >= threshold or \
+                                    string_sim(binding['altLabel']['value'], entity, dist_type) >= threshold: filter_results.append(binding)
+              else:
+                     for binding in results['bindings']:
+                            if string_sim(binding['prefLabel']['value'], entity, dist_type) <= threshold or \
+                                    string_sim(binding['altLabel']['value'], entity, dist_type) <= threshold: filter_results.append(binding)
        return filter_results
 
 
 
 # Example
 
+
+# similerities
+
+print(edit_sim("python programming", "programmer"))
+print(levenshtein_sim("python programming", "programmer"))
+print(jaccard_index("python programming", "programmer"))
+print(jaro.jaro_winkler_metric("python programming", "programmer"))
+
+
+
 # query ttl file skill/occupetion
+
 x = """
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
 PREFIX esco: <http://ec.europa.eu/esco/model#>      
@@ -64,16 +124,14 @@ SELECT ?skill ?prefLabel ?altLabel ?hiddenLabel
 WHERE {     
     ?skill skos:prefLabel ?prefLabel .
     ?skill skos:altLabel ?altLabel .
-    ?skill skos:hiddenLabel ?hiddenLabel .
-    
-    
+    ?skill skos:hiddenLabel ?hiddenLabel .   
 }
-
 """
 
 
 # query set of 3 ttl little files
-z = """
+
+y = """
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
 PREFIX esco: <http://ec.europa.eu/esco/model#>      
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>   
@@ -81,22 +139,43 @@ SELECT ?skill ?prefLabel ?altLabel
 WHERE {     
     ?skill skos:prefLabel ?prefLabel .
     ?skill skos:altLabel ?altLabel .
-
 }
-
 """
 
-# THIS SEEMS TO WORK!!!
-y = """SELECT ?subject ?predicate ?object
-WHERE {
-  ?subject ?predicate ?object
 
-}"""
+# get output -> into pickle
 
-output = sparql_query(x, "http://localhost:3030/ds")
-res = eval_results(output, "writing Punjabi", 10, "levenshtein")
+'''output = sparql_query(x, "http://localhost:3030/ds")
+file_x = open("skill_occupation.pickle", "wb")
+pickle.dump(output, file_x)
+file_x.close()'''
+
+'''output = sparql_query(y, "http://localhost:3030/ds")
+file_y = open("skill_digital_language.pickle", "wb")
+pickle.dump(output, file_y)
+file_y.close()'''
+
+
+# load pickle
+
+file_x = open("skill_occupation.pickle", "rb")
+skill = pickle.load(file_x)
+print(skill)
+
+file_y = open("skill_digital_language.pickle", "rb")
+skill_digital_language = pickle.load(file_y)
+print(skill_digital_language)
+
+
+# evaluate similarity
+
+res = eval_results(skill, "writing Punjabi", ">=", 0.7, "levenshtein", 1)
 print(res)
 print(len(res))
+
+res1 = eval_results(skill_digital_language, "writing Punjabi", ">=", 0.7, "levenshtein", 2)
+print(res1)
+print(len(res1))
 
 
 
