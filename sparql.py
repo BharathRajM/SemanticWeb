ce5a1b0df30c921a2bf937d0b10d07ae9d1f93ff
vorsanigo <verorsanigo@gmail.com>
2021-11-06 11:21:18 +0100

    added function for evaluation

--- sparql.py
+++ sparql.py
@@ -27,7 +27,7 @@ def sparql_query(query, endpoint):
        sparql.setQuery(query)
        sparql.setReturnFormat(JSON)
        output = sparql.query().convert()
-       #print(output)
+
        return output
 
 
@@ -113,7 +113,6 @@ def eval_results(output, entity, compare, threshold, dist_type, taxonomy_type):
        '''
 
        results = output['results']
-       #print(results)
 
        filter_results = []
        filter_uri = []
@@ -134,6 +133,7 @@ def eval_results(output, entity, compare, threshold, dist_type, taxonomy_type):
                                     string_sim(binding['hiddenLabel']['value'].lower(), entity.lower(), dist_type) <= threshold:
                                    filter_results.append(binding)
                                    filter_uri.append(binding['skill']['value'])
+
        else: # files ict_skills_collection.ttl or language_skills_collection.ttl or transversal_skills_collection.ttl
               if compare == ">=":
                      for binding in results['bindings']:
@@ -147,7 +147,62 @@ def eval_results(output, entity, compare, threshold, dist_type, taxonomy_type):
                                     string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type) <= threshold:
                                    filter_results.append(binding)
                                    filter_uri.append(binding['skill']['value'])
+
        return list(set(filter_uri)) # if needed we can use filter_results
+
+
+def eval_results_more_sim(output, entity, compare, threshold_1, threshold_2, dist_type_1, dist_type_2, taxonomy_type):
+       '''
+       Given the output containing all the labels from the taxonomy section of skills and occupations, the entity (string)
+       to match and a threshold, it returns the labels with the related skill URI that have similarity with the entity
+       below the threshold, taxonomy_type (1,2) indicates if we consider the section of occupations/skills or of different
+       skills
+       :param output:
+       :param entity:
+       :param compare:
+       :param threshold:
+       :param dist_type:
+       :param taxonomy_type:
+       :return:
+       '''
+
+       results = output['results']
+
+       filter_results_1 = []
+       filter_uri_1 = []
+
+       filter_results_2 = []
+       filter_uri_2 = []
+
+
+       if taxonomy_type == 1: # files esco_occupation.ttl or esco_skill.ttl
+              if compare == ">=":
+                     for binding in results['bindings']:
+                            if string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type_1) >= threshold_1 or \
+                                    string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type_1) >= threshold_1 or \
+                                    string_sim(binding['hiddenLabel']['value'].lower(), entity.lower(), dist_type_1) >= threshold_1:
+                                   filter_results_1.append(binding)
+                                   filter_uri_1.append(binding['skill']['value'])
+                            if string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type_2) >= threshold_2 or \
+                                    string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type_2) >= threshold_2 or \
+                                    string_sim(binding['hiddenLabel']['value'].lower(), entity.lower(), dist_type_2) >= threshold_2:
+                                   filter_results_2.append(binding)
+                                   filter_uri_2.append(binding['skill']['value'])
+
+
+       else: # files ict_skills_collection.ttl or language_skills_collection.ttl or transversal_skills_collection.ttl
+              if compare == ">=":
+                     for binding in results['bindings']:
+                            if string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type_1) >= threshold_1 or \
+                                    string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type_1) >= threshold_1:
+                                   filter_results_1.append(binding)
+                                   filter_uri_1.append(binding['skill']['value'])
+                            if string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type_2) >= threshold_2 or \
+                                    string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type_2) >= threshold_2:
+                                   filter_results_2.append(binding)
+                                   filter_uri_2.append(binding['skill']['value'])
+
+       return list(set(filter_uri_1)), list(set(filter_uri_2)) # if needed we can use filter_results
 
 
 def eval_results_uri_occupation(output, entity, compare, threshold, dist_type):
@@ -162,7 +217,6 @@ def eval_results_uri_occupation(output, entity, compare, threshold, dist_type):
        '''
 
        results = output['results']
-       #print(results)
 
        filter_uri_tot = []
        filter_uri = []
@@ -174,14 +228,11 @@ def eval_results_uri_occupation(output, entity, compare, threshold, dist_type):
                      sim2 = string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type)
                      sim3 = string_sim(binding['hiddenLabel']['value'].lower(), entity.lower(), dist_type)
                      if sim1 >= threshold or sim2 >= threshold or sim3 >= threshold:
-                            '''print("sim1", sim1)
-                            print("sim2", sim2)
-                            print("sim3", sim3)'''
                             filter_uri_tot.append(binding['skill']['value'])
                             if binding['skill']['value'] not in filter_uri:
                                    filter_uri.append(binding['skill']['value'])
                                    scores.append(max(sim1, sim2, sim3))
-                                   #print("scores", scores)
+
        else:
               for binding in results['bindings']:
                      sim1 = string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type)
@@ -195,13 +246,66 @@ def eval_results_uri_occupation(output, entity, compare, threshold, dist_type):
 
        if len(scores) != 0:
               max_score = max(scores)
-              print("max_score", max_score)
               max_pos = scores.index(max_score)
-              print("max pos", max_pos)
               filter_uri = [filter_uri[max_pos]]
-              print(filter_uri)
 
        return filter_uri_tot, filter_uri # if needed we can use filter_results
+
+
+def eval_results_uri_occupation_more_sim(output, entity, compare, threshold_1, threshold_2, dist_type_1, dist_type_2):
+       '''
+       This is one variation of eval_results_uri_occupation to consider 2 different similarity types (for faster evaluation)
+       :param output:
+       :param entity:
+       :param compare:
+       :param threshold:
+       :param dist_type:
+       :return:
+       '''
+
+       results = output['results']
+
+       filter_uri_tot_1 = []
+       filter_uri_1 = []
+       scores_1 = []
+
+       filter_uri_tot_2 = []
+       filter_uri_2 = []
+       scores_2 = []
+
+       if compare == ">=":
+              for binding in results['bindings']:
+                     sim1_1 = string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type_1)
+                     sim2_1 = string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type_1)
+                     sim3_1 = string_sim(binding['hiddenLabel']['value'].lower(), entity.lower(), dist_type_1)
+                     if sim1_1 >= threshold_1 or sim2_1 >= threshold_1 or sim3_1 >= threshold_1:
+                            filter_uri_tot_1.append(binding['skill']['value'])
+                            if binding['skill']['value'] not in filter_uri_1:
+                                   filter_uri_1.append(binding['skill']['value'])
+                                   scores_1.append(max(sim1_1, sim2_1, sim3_1))
+
+                     sim1_2 = string_sim(binding['prefLabel']['value'].lower(), entity.lower(), dist_type_2)
+                     sim2_2 = string_sim(binding['altLabel']['value'].lower(), entity.lower(), dist_type_2)
+                     sim3_2 = string_sim(binding['hiddenLabel']['value'].lower(), entity.lower(), dist_type_2)
+                     if sim1_2 >= threshold_2 or sim2_2 >= threshold_2 or sim3_2 >= threshold_2:
+                            filter_uri_tot_2.append(binding['skill']['value'])
+                            if binding['skill']['value'] not in filter_uri_2:
+                                   filter_uri_2.append(binding['skill']['value'])
+                                   scores_2.append(max(sim1_2, sim2_2, sim3_2))
+
+
+       if len(scores_1) != 0:
+              max_score_1 = max(scores_1)
+              max_pos_1 = scores_1.index(max_score_1)
+              filter_uri_1 = [filter_uri_1[max_pos_1]]
+
+       if len(scores_2) != 0:
+              max_score_2 = max(scores_2)
+              max_pos_2 = scores_2.index(max_score_2)
+              filter_uri_2 = [filter_uri_2[max_pos_2]]
+
+       return filter_uri_tot_1, filter_uri_1, filter_uri_tot_2, filter_uri_2 # if needed we can use filter_results
+
 
 
 def eval_results_tot(output, list_entities, compare, threshold, dist_type, taxonomy_type): # maybe instead of list entities we have key-value(resume/job proposal: list entities)
@@ -221,9 +325,32 @@ def eval_results_tot(output, list_entities, compare, threshold, dist_type, taxon
        for e in list_entities:
               filter_uri = eval_results(output, e, compare, threshold, dist_type, taxonomy_type)
               matches = (set(matches) | set(filter_uri)) # set not needed bcs already sets
-       #score = len(matches)
 
        return matches # list of matches (uris)
+
+
+def eval_results_tot_more_sim(output, list_entities, compare, threshold_1, threshold_2, dist_type_1, dist_type_2, taxonomy_type): # maybe instead of list entities we have key-value(resume/job proposal: list entities)
+       '''
+       Given the output from the query, the list of retrieved entities, it finds the matches (uris without duplicates
+       for the entities inside the taxonomy using string similarity
+       :param output:
+       :param list_entities:
+       :param compare:
+       :param threshold:
+       :param dist_type:
+       :param taxonomy_type:
+       :return:
+       '''
+
+       matches_1 = []
+       matches_2 = []
+       for e in list_entities:
+              filter_uri = eval_results_more_sim(output, e, compare, threshold_1, threshold_2, dist_type_1, dist_type_2, taxonomy_type)
+              matches_1 = (set(matches_1) | set(filter_uri[0])) # set not needed bcs already sets
+              matches_2 = (set(matches_2) | set(filter_uri[1]))
+
+       return matches_1, matches_2 # lists of matches (uris)
+
 
 
 def compute_score(output_ess_opt, list_uri_skills_resume, list_uri_skills_job_proposal, list_uri_occupations_resume, list_uri_occupations_job, list_uri_occupations_job_filter):
@@ -246,55 +373,36 @@ def compute_score(output_ess_opt, list_uri_skills_resume, list_uri_skills_job_pr
        score = 0
 
        # score given by if resume title and job title corresponds
-       print("list_uri_occupations_job", list_uri_occupations_job)
-       print("list_uri_occupations_resume", list_uri_occupations_resume)
        if len(set(list_uri_occupations_resume).intersection(set(list_uri_occupations_job))) != 0:
-              print("plus 3")
               score += 3
 
        # score given by entities from resume and job proposal mapping to same entities into the taxonomy
        for el in list_uri_skills_resume: # retrieved from all the 5 skills ttl files
               if el in list_uri_skills_job_proposal:
-                     print("plus 1")
                      score += 1  # if resume's skill and job proposal's map to same entity in the taxonomy
 
        set_matching = set()
        set_matching_2 = set()
+
        # score given by entities from resume that map to essential/optional skills for job proposal's skills
-       print("list uri", list_uri_skills_job_proposal)
        for binding in results_ess_opt['bindings']:
               for el in list_uri_skills_resume:
-                     #print("ellll", el)
-                     #if len(binding['essential']['value']) >= 33 and binding['essential']['value'][0:33] == 'http://data.europa.eu/esco/skill/':
-                     '''if binding['skill']['value'] == el:
-                            print("qqqqqqqqqqq", binding['skill']['value'])
-                     if binding['essential']['value'] == "http://data.europa.eu/esco/occupation/9ebaf3f0-0be0-47b7-b2b1-b3b04130fa81":
-                            print("ddddddddd", binding['essential']['value'])'''
                      if el == binding['skill']['value'] and binding['essential']['value'] in list_uri_skills_job_proposal:
-                            print("wwwwwwwwwwwww")
                             if el not in set_matching:
-                                   print("sssssssssss", binding['skill']['value'], binding['essential']['value'])
                                    score += 0.5 # if mapped skill from resume is essential skill for a skill required by job proposal
                                    set_matching.add(el)
                      elif el == binding['skill']['value'] and binding['optional']['value'] in list_uri_skills_job_proposal:
-                            print("zzzzzzzzzzzzzzzzzzzzzzzzzz")
                             if el not in set_matching:
-                                   print("ssssssssssss1", binding['skill']['value'], binding['optional']['value'])
                                    score += 0.25 # if mapped skill from resume is optional skill for a skill required by job proposalset_matching.add(el)
                                    set_matching.add(el)
 
        # score given by entities from resume that map to essential/optional skills for job proposal's occupation
-       #for binding in results_ess_opt['bindings']:
-              #for el in list_uri_occupations_job:
-                     #if len(binding['essential']['value']) >= 33 and binding['essential']['value'][0:38] == 'http://data.europa.eu/esco/occupation/':
                      if el == binding['skill']['value'] and binding['essential']['value'] in list_uri_occupations_job_filter:
                             if el not in set_matching_2:
-                                   print("CIAO")
                                    score += 0.5  # if mapped skill from resume is essential skill for the job (occupation)
                                    set_matching_2.add(el)
                      elif el == binding['skill']['value'] and binding['optional']['value'] in list_uri_occupations_job_filter:
                             if el not in set_matching_2:
-                                   print("CIAO1", binding['skill']['value'], binding['optional']['value'])
                                    score += 0.25  # if mapped skill from resume is optional skill for the job (occupation)
                                    set_matching_2.add(el)
 
